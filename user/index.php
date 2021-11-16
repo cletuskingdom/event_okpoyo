09307008c2d41930c990736d5af3063f705256f1
Cletus Kingdom <cletuskingdom@gmail.com>
2021-11-16 16:45:02 +0100

    user can see qrcode

--- user/index.php
+++ user/index.php
@@ -56,8 +56,7 @@
 									<?php
 										$i = 1;
 										$username = $_SESSION['userdata']['username'];
-										// $qry = $conn->query("SELECT * from event_audience INNER JOIN event_list on event_audience.event_id=event_list.id and event_audience.name='$username'");
-										$qry = $conn->query("SELECT a.*,e.title FROM event_audience a inner join event_list e on e.id = a.event_id order by a.name asc  ");
+										$qry = $conn->query("SELECT * from event_audience INNER JOIN event_list on event_audience.event_id=event_list.id and event_audience.name='$username'");
 										while($row = $qry->fetch_assoc()):
 									?>
 									<tr>
